635e1b409524ac3bab0dc91a4ed2b9ef0d9fbe0f
lucy <lucy.brandao@znc.com.br>
2020-06-06 22:31:46 -0300

    Auth
    Criada autenticação do sistema
    
    instaladas djangorestframework (configurar api rest no django),
    djangorestframework-simplejwt (configurar autenticação no djangorestframework)
    e django-cors-headers (configura permissão de conecção com api).

--- blogueirinhos/urls.py
+++ blogueirinhos/urls.py
@@ -15,9 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
 from base import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', views.hello_world),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
